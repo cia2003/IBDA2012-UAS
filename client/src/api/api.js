6ee61c96cd5247867f16635accc843b16cc74422
Jocalvin <calvinjoshua37@gmail.com>
2026-05-03 10:12:23 +0700

    Tambah Axios

---
+++ client/src/api/api.js
@@ -0,0 +1,13 @@
+// import axios from "axios";
+
+// const api = () => {
+//   const api = axios.create({
+//     baseURL: "https://api.example.com", // Ganti URL nya nanti
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//   });
+// };
+
+// export default api
