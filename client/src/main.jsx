ab240237bf3c692ae9399c4725d4b07275546f0d
Jocalvin <calvinjoshua37@gmail.com>
2026-05-06 10:54:57 +0700

    Buat context global untuk user

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,15 +5,18 @@ import { BrowserRouter } from "react-router-dom";
 import { AppContextProvider } from "./context/AppContext.jsx";
 import { ManagerContextProvider } from "./context/ManagerContext.jsx";
 import { StaffContextProvider } from "./context/StaffContext.jsx";
+import { UserContextProvider } from "./context/UserContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AppContextProvider>
       <ManagerContextProvider>
         <StaffContextProvider>
-          <StrictMode>
-            <App />
-          </StrictMode>
+          <UserContextProvider>
+            <StrictMode>
+              <App />
+            </StrictMode>
+          </UserContextProvider>
         </StaffContextProvider>
       </ManagerContextProvider>
     </AppContextProvider>
